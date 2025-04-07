d8a34fa4756482467279023883748cc07b355bf6
Fabian Lemus <167130769+Viper12CU@users.noreply.github.com>
2025-04-07 10:18:36 -0400

    Renombrar imagenes y correccion de rutas

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ document.addEventListener('DOMContentLoaded', function () {
             if (isLeft) {
                 innerDiv.classList.add('right');
                 imgPoster.classList.add("right");
-                imgPoster.src = "/lib/Sign in nuevo.jpg"
+                imgPoster.src = "/lib/Sign-in.jpg"
 
             } else {
                 innerDiv.classList.remove('right');
                 imgPoster.classList.remove("right");
-                imgPoster.src = "/lib/Login nuevo.jpg";
+                imgPoster.src = "/lib/Login.jpg";
                 // Log in
 
             }
